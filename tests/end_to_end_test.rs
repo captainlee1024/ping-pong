26bdb6507ef2dba1df20d46a9264afab9305211b
Terry <644052732@qq.com>
2024-03-29 21:28:46 +0800

    test: update mock test

--- tests/end_to_end_test.rs
+++ tests/end_to_end_test.rs
@@ -1,12 +1,16 @@
-use pingpong::pingpongcs::client::{PingPongClient, PingPongClientHandler};
-use pingpong::pingpongcs::server::{launch_scheduler_server_with_addr, PingPongServiceHandler};
+use pingpong::pingpongcs::client::{MyPingPongClientHandler, PingPongClient, PingPongClientHandler};
+use pingpong::pingpongcs::server::{launch_scheduler_server_with_addr, PingPongHandler, PingPongServiceHandler};
 use std::sync::Arc;
+use tonic::async_trait;
+use pingpong::pingpongcs::client::pingpong::{PingRequest as ClientPingRequest, PongResponse as ClientPongResponse};
+use pingpong::pingpongcs::server::pingpong::{PingRequest as ServerPingRequest, PongResponse as ServerPongResponse};
+
 
 #[tokio::test]
 async fn test_ping_pong_stream_e2e() {
     // Start the server
     let server_addr = "[::1]:50051".to_string();
-    let ping_pong_handler = Arc::new(PingPongServiceHandler::default());
+    let ping_pong_handler = Arc::new(MockPingPongServiceHandler::default());
     let server_future = tokio::spawn(async move {
         launch_scheduler_server_with_addr(server_addr, ping_pong_handler)
             .await
@@ -18,7 +22,7 @@ async fn test_ping_pong_stream_e2e() {
 
     // Run the client
     let client_addr = "http://[::1]:50051".to_string();
-    let handler = Arc::new(PingPongClientHandler::default());
+    let handler = Arc::new(MockPingPongClientHandler::default());
     let client = PingPongClient::new(client_addr, handler);
     let client_future = tokio::spawn(async move {
         client.launch_service().await.unwrap();
@@ -30,4 +34,41 @@ async fn test_ping_pong_stream_e2e() {
 
     // Stop the server
     server_future.abort();
+}
+
+#[derive(Default)]
+pub struct MockPingPongServiceHandler {}
+
+
+#[async_trait]
+impl PingPongHandler for MockPingPongServiceHandler {
+    async fn handle_ping(&self, ping_request: ServerPingRequest) -> ServerPongResponse {
+        if ping_request.message != "ping" {
+            return ServerPongResponse {
+                message: format!("unexpect message {}", ping_request.message),
+            };
+        }
+
+        ServerPongResponse {
+            message: "pong".to_string(),
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct MockPingPongClientHandler {}
+
+#[async_trait]
+impl MyPingPongClientHandler for MockPingPongClientHandler {
+    async fn handle_ping(&self, i: i32, pong_response: ClientPongResponse) -> ClientPingRequest {
+        if pong_response.message != "pong" {
+            return ClientPingRequest {
+                message: format!("unexpect message {}", pong_response.message),
+            };
+        }
+
+        ClientPingRequest {
+            message: format!("ping{}", i),
+        }
+    }
 }
